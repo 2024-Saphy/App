b3054479b22f52d5dd31b1fbf8d777622afa9c45
cho4u4o <cho4u4o@gmail.com>
2024-07-20 21:08:02 +0900

    Feat : 탐색 화면, 하단 네비바 추가

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -9,17 +9,17 @@ import 'package:saphy/widgets/product_card.dart';
 void main() {
   // async {
   // await dotenv.load(fileName: 'assets/config/.env');
-  runApp(const HomePage());
+  runApp(const MainScreen());
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<MainScreen> createState() => _MainScreenState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _MainScreenState extends State<MainScreen> {
   final NumberFormat numberFormat = NumberFormat('###,###,###,###');
   List<Product> productList = [
     // 그냥 구현용 샘플 데이터
@@ -67,107 +67,109 @@ class _HomePageState extends State<HomePage> {
       title: "Saphy",
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: const Color(0xfff4f4f4),
-          appBar: AppBar(
-              backgroundColor: Colors.white,
-              title: Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Image.asset(
-                  "assets/images/SaphyLogoSmall.png",
-                  height: 24,
+        backgroundColor: const Color(0xfff4f4f4),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Image.asset(
+              "assets/images/SaphyLogoSmall.png",
+              height: 24,
+            ),
+          ),
+          centerTitle: false,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: IconButton(
+                icon: const Icon(Icons.notifications_outlined),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+        body: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Container(
+                height: 280,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: CachedNetworkImageProvider(
+                        "https://i.pinimg.com/564x/56/28/6f/56286feaa430d8d83dbe9382f8c10c8d.jpg",
+                      ),
+                      fit: BoxFit.cover),
                 ),
               ),
-              centerTitle: false,
-              actions: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 20.0),
-                  child: IconButton(
-                    icon: const Icon(Icons.notifications_outlined),
-                    onPressed: () {},
-                  ),
-                ),
-              ]),
-          body: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
+            ),
+            const SliverToBoxAdapter(
+              child: SizedBox(
+                height: 20,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Container(
-                  height: 280,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: CachedNetworkImageProvider(
-                          "https://i.pinimg.com/564x/56/28/6f/56286feaa430d8d83dbe9382f8c10c8d.jpg",
+                  height: 100,
+                  decoration: BoxDecoration(
+                      color: const Color(0xffDFE1E6),
+                      borderRadius: BorderRadius.circular(30)),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 20.0,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "중고기기를 안전하게 구매하는 방법",
+                          style: TextStyle(
+                            fontFamily: "Pretendard",
+                            fontSize: 15,
+                          ),
                         ),
-                        fit: BoxFit.cover),
-                  ),
-                ),
-              ),
-              const SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 20,
-                ),
-              ),
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                        color: const Color(0xffDFE1E6),
-                        borderRadius: BorderRadius.circular(30)),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 30.0,
-                        vertical: 20.0,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "중고기기를 안전하게 구매하는 방법",
-                            style: TextStyle(
+                        Text(
+                          "사피 초대하고 지원금 받기",
+                          style: TextStyle(
                               fontFamily: "Pretendard",
-                              fontSize: 15,
-                            ),
-                          ),
-                          Text(
-                            "사피 초대하고 지원금 받기",
-                            style: TextStyle(
-                                fontFamily: "Pretendard",
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
-              SliverPadding(
-                sliver: SliverGrid(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                      return productCard(
-                          productBrand: productList[index].productBrand ?? "",
-                          productName: productList[index].productName ?? "",
-                          productImageUrl:
-                              productList[index].productImageUrl ?? "",
-                          price: productList[index].price ?? 0);
-                    },
-                    childCount: productList.length,
-                  ),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 15,
-                    crossAxisSpacing: 15,
-                    childAspectRatio: 0.84,
-                  ),
+            ),
+            SliverPadding(
+              sliver: SliverGrid(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return productCard(
+                        productBrand: productList[index].productBrand ?? "",
+                        productName: productList[index].productName ?? "",
+                        productImageUrl:
+                            productList[index].productImageUrl ?? "",
+                        price: productList[index].price ?? 0);
+                  },
+                  childCount: productList.length,
                 ),
-                padding: const EdgeInsets.all(20.0),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 15,
+                  crossAxisSpacing: 15,
+                  childAspectRatio: 0.84,
+                ),
               ),
-            ],
-          ),
-          bottomNavigationBar: bottomNavBar()),
+              padding: const EdgeInsets.all(20.0),
+            ),
+          ],
+        ),
+        bottomNavigationBar: const BottomNavBar(),
+      ),
     );
   }
 }
