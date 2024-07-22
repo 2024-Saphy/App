34775ca971777804dd4131b5bbf5e5ca0bb1ca1c
이원희 <story3798@naver.com>
2024-07-22 17:58:44 +0900

    Feat : 마이페이지

---
+++ lib/screens/mypage/mypage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatefulWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Page"),
+      ),
+      body: const Center(
+        child: Text("My Page"),
+      ),
+    );
+  }
+}
