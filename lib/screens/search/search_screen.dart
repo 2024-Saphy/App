8225d1907f0dfb894258cd1306fa8353e324fc0b
cho4u4o <cho4u4o@gmail.com>
2024-07-22 23:54:02 +0900

    SAPHY-109 Feat : 네비바 방식 변경, 스크린 컨트롤러 생성, 화면 수정

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,92 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:saphy/widgets/bottom_nav_bar.dart';
-import 'package:saphy/screens/main/main_screen.dart';
+import 'package:saphy/widgets/text_field.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xfff4f4f4),
+    return const Scaffold(
+      backgroundColor: Color(0xfff4f4f4),
       body: Column(
         children: [
-          const SizedBox(
+          SizedBox(
             height: 50,
           ),
           Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(20.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.arrow_back),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const MainScreen(),
-                          ),
-                        );
-                      },
-                    ),
-                    SizedBox(
-                      width: 290,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          labelStyle: const TextStyle(
-                            backgroundColor: Colors.white,
-                          ),
-                          suffixIcon: IconButton(
-                            icon: const Icon(Icons.clear),
-                            onPressed: () {},
-                            // 컨트롤러 연결해야 해서 아직 미구현
-                          ),
-                          filled: true,
-                          fillColor: Colors.white,
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                            borderSide: const BorderSide(
-                              color: Color(0xff272727),
-                            ),
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                            borderSide: const BorderSide(
-                              color: Color.fromARGB(255, 83, 83, 83),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.search),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-                const SizedBox(
+                CustomTextField(),
+                SizedBox(
                   height: 20,
                 ),
                 // 아래 모두 데이터 연동 & stateful 위젯으로의 전환 추후 진행예정
-                const Text(
+                Text(
                   "최근 검색어",
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                 ),
-                const SizedBox(
+                SizedBox(
                   height: 20,
                 ),
-                const Text(
+                Text(
                   "인기 검색어",
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                 ),
-                const SizedBox(
+                SizedBox(
                   height: 20,
                 ),
-                const Text(
+                Text(
                   "인기 브랜드",
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                 ),
@@ -95,7 +47,6 @@ class SearchScreen extends StatelessWidget {
           ),
         ],
       ),
-      bottomNavigationBar: const BottomNavBar(),
     );
   }
 }
