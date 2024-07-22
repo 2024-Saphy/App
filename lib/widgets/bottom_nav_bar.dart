8f574d3cecdd17c3e807f055657a650e2bbf98e4
cho4u4o <cho4u4o@gmail.com>
2024-07-23 01:05:52 +0900

    SAPHY-109 Design : 네비바 색 변경

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -14,6 +14,7 @@ class BottomNavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: currentIndex,
+      backgroundColor: Colors.white,
       selectedItemColor: const Color(0xff9abcff),
       unselectedItemColor: Colors.black,
       onTap: onTap,
