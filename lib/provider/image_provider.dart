be7348995542fd8d73fe1ae8d5783b6b00db3af7
boroboro01 <duke7201@naver.com>
2024-10-15 15:34:29 +0900

    feat: create provider for image

---
+++ lib/provider/image_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageProviderModel with ChangeNotifier {
+  final List<XFile> _images = [];
+
+  List<XFile> get images => _images;
+
+  Future<void> addImage(XFile image) async {
+    _images.add(image);
+    notifyListeners(); // 상태가 변경되었음을 알림
+  }
+
+  void removeImage(int index) {
+    _images.removeAt(index);
+    notifyListeners();
+  }
+}
